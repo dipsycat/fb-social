2c3a0b31ca0caa3ec3532e200ad7f06ab179c2ff
dipsycat <dipsycat@yandex.ru>
2017-03-22 23:55:31 +0300

    intergate fos js routing bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new KnpU\GuardBundle\KnpUGuardBundle(),
             new IAkumaI\SphinxsearchBundle\SphinxsearchBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/Dipsycat/FbSocialBundle/Resources/public/dist/js/search.js
@@ -0,0 +1,26 @@
+$('#search-btn').click(function () {
+
+});
+$('#search-text').keyup(function () {
+    var url = Routing.generate('dipsycat_fb_social_friend_search');
+    var data = {
+        'search_text': $(this).val()
+    };
+    if ($(this).val().length == 1) {
+        $.ajax({
+            type: 'get',
+            url: url,
+            data: data
+        })
+            .done(function (response) {
+                if (response.result == 'success') {
+                    var users = response.data;
+                    $('datalist#users').empty();
+                    for (var item in users) {
+                        $('datalist#users').append('<option value="' + users[item] + '"></option>');
+                    }
+                }
+            });
+    }
+
+});
